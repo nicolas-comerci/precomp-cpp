ec1bf36adf28b0093168be7f3ec5f24d7418b9af
Nicolás Comerci <nicolas.comerci@fing.edu.uy>
2023-02-23 02:50:27 -0300

    Idem SWF

---
+++ formats/swf.cpp
@@ -0,0 +1,34 @@
+#include "swf.h"
+
+#include "zlib.h"
+
+bool swf_header_check(const unsigned char* checkbuf) {
+  // CWS = Compressed SWF file
+  auto cws_hdr = (*checkbuf == 'C') && (*(checkbuf + 1) == 'W') && (*(checkbuf + 2) == 'S');
+  return cws_hdr && zlib_header_check(checkbuf + 8);
+}
+
+deflate_precompression_result try_decompression_swf(Precomp& precomp_mgr) {
+  deflate_precompression_result result = deflate_precompression_result(D_SWF);
+  //int windowbits = (precomp_mgr.ctx->in_buf[precomp_mgr.ctx->cb + 8] >> 4) + 8;
+
+  precomp_mgr.ctx->saved_input_file_pos = precomp_mgr.ctx->input_file_pos;
+  precomp_mgr.ctx->saved_cb = precomp_mgr.ctx->cb;
+
+  precomp_mgr.ctx->input_file_pos += 10; // skip CWS and zLib header
+
+  result = try_decompression_deflate_type(precomp_mgr, precomp_mgr.statistics.decompressed_swf_count, precomp_mgr.statistics.recompressed_swf_count,
+    D_SWF, precomp_mgr.ctx->in_buf + precomp_mgr.ctx->cb + 3, 7, true,
+    "in SWF", temp_files_tag() + "_original_swf");
+
+  precomp_mgr.ctx->cb += 10;
+  return result;
+}
+
+void recompress_swf(Precomp& precomp_mgr, unsigned char precomp_hdr_flags) {
+  precomp_mgr.ctx->fout->put('C');
+  precomp_mgr.ctx->fout->put('W');
+  precomp_mgr.ctx->fout->put('S');
+
+  recompress_deflate(precomp_mgr, precomp_hdr_flags, true, temp_files_tag() + "_recomp_swf", "SWF");
+}
